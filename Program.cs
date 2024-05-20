6aa5c50f808f4034263207317d18e419ec51f272
Ted Collins <theodore.collins2023@outlook.com>
2024-05-20 14:25:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment3._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = GetString();
+            CountBlankSpaces(sentence);
+        }
+
+        public static string GetString()
+        {
+            string userInput = "test";
+            do
+            {
+                Console.WriteLine("Please enter a sentence and the program will tell you how many blank spaces it contains.");
+                userInput = Console.ReadLine();
+            } while (userInput == "");
+
+            return userInput;
+        }
+
+        public static void CountBlankSpaces(string sentence)
+        {
+            int counter = 0;
+            foreach (char c in sentence)
+            {
+                if (c == ' ') counter++;
+            }
+            Console.WriteLine($"\"{sentence}\" contains {counter} blank spaces");
+        }
+    }
+}
